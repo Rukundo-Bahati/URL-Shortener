9c13b93818a1f8f60d2d050ffa4095d33b09cb86
RUKUNDO BAHATI Sam <rukundorca@gamil.com>
2024-05-08 14:13:38 +0200

    adding login link in main page

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports = function(req,res,next) {
   const token  = req.header('x-auth-token')
-  if(!token) return res.status(401).send('Access Denied!. No token provided')
+  if(!token) return res.status(401).send('<h2>Access Denied!. No token provided <br /> login or sign up if you have no account.</h2>')
 
     try {
       const decoded = jwt.verify(token, process.env.JWTPRIVATEKEY)
